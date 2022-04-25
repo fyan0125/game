376fb21f07b02379f3d5eabe17a86df537c0ced3
cianyastone <lincofe@gmail.com>
2022-04-25 14:02:58 +0800

    Generate Enemy

---
+++ Assets/Script/Controllers/GenerateEnemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemy : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    public int maxEnemyCount;
+
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop(){
+        while(enemyCount < maxEnemyCount){
+            xPos = Random.Range(-9, 4);
+            zPos = Random.Range(-11, -2);
+            Instantiate(theEnemy, new Vector3(xPos, -4, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1; 
+        }
+    }
+
+}
