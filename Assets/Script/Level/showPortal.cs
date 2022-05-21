a1642339ae245dbf6dfdc4df88686ecf04d28c84
cianyastone <lincofe@gmail.com>
2022-05-22 00:45:03 +0800

    changeScene by clear

--- Assets/Script/Level/showPortal.cs
+++ Assets/Script/Level/showPortal.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class showPortal : MonoBehaviour
 {
+    public GameObject Portal;
+    
+    public bool isClear;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isClear = false;
+        //Portal.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isClear){
+            Portal.SetActive(true);
+        }else if(!isClear){
+            Portal.SetActive(false);
+        }
     }
 }
