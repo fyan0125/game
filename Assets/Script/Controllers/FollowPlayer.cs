bf4eac893c58bad6f427056a7021e33bbdb6024d
cianyastone <lincofe@gmail.com>
2022-10-23 13:44:25 +0800

    add fox

--- Assets/Script/Controllers/FollowPlayer.cs
+++ Assets/Script/Controllers/FollowPlayer.cs
@@ -3,42 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
-{/*
+{
     // Start is called before the first frame update
     public Transform player;
     public LayerMask whatIsPlayer;
     public float sightRange;
     public bool playerInSightRange;
     Transform target;
-    public Animator anim;
+    private Animator anim;
     public UnityEngine.AI.NavMeshAgent agent;
     public GameObject nowFollowing;
+    private GameObject gameManager;
+    private attendantManager aM;
+    public GameObject[] npcs = new GameObject[7];
+    private npcRabbit nR;
 
 
     void Start()
     {
         target = player.transform;
+        aM = gameManager.GetComponent<attendantManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(aM.rabbitArea.activeSelf){
+            nowFollowing = npcs[0];
+            playerFollow();
+        }
         //Following Player
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        if (!playerInSightRange)
-        {
-            playerFollow();
-            nowFollowing.anim.SetBool("isWalking", true);
-        }
-        else if (playerInSightRange)
-        {
-            nowFollowing.anim.SetBool("isWalking", false);
-        }
     }
     public void playerFollow()
     {
-        agent.speed = 2;
-        agent.destination = target.position;
-    }*/
+        if (!playerInSightRange)
+        {
+            //nowFollowing.anim.SetBool("isWalking", true);
+            agent.speed = 2;
+            agent.destination = target.position;
+        }
+        else if (playerInSightRange)
+        {
+           // nowFollowing.anim.SetBool("isWalking", false);
+        }
+    }
 }
